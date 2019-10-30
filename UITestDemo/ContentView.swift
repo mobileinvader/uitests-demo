2b464583f019d84457e64fae7c8d212ac28a5aa6
Kristijan Kralj <kristijan.kralj@live.com>
2019-10-30 06:46:06 +0100

    Initial Commit

---
+++ UITestDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UITestDemo
+//
+//  Created by Kristijan Kralj on 30/10/2019.
+//  Copyright © 2019 Kristijan Kralj. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
